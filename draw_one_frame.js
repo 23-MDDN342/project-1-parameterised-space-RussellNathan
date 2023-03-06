b66209baf5fb03d75d6c3c8f874370b13611d24d
Nathan Russell <russelnath2@myvuw.ac.nz>
2023-03-07 11:23:11 +1300

    Window
    Added the window element with a similar sin movement to the cursor on the y axis.

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,15 +1,34 @@
+let t = 0.1;
+let trail = 144;
+let errors;
+let td = 0;
+
+function preload() {
+	errors = loadStrings('console');
+}
 
 function draw_one_frame(cur_frac) {
+	background('#55abe3');
+
+	strokeWeight(2);
+	
+	//window
+	for(let i = 0; i <= trail; i++){
+		let ghost_frac = (cur_frac+=1/trail)%1;
+		if (i == trail) {td = 1;} else {td = 0;}
+		drawWindow(100,100+ sin(ghost_frac*TWO_PI)*100,td);
+	}
+
+	// blendMode(OVERLAY);
+
 	//cursor
 	strokeCap(PROJECT);
 	strokeJoin(MITER);
 	fill("white");
 	stroke("black");
-	strokeWeight(2);
-	for(let i = 0; i < 12; i++){
-		let ghost_frac = (cur_frac+=1/48)%1
-		// (ghost_frac+=1/48)%1
-		let cur_x = 200+200*sin(ghost_frac*PI);
+	for(let i = 0; i <= t*trail; i++){
+		let ghost_frac = (cur_frac+=1/trail)%1;
+		let cur_x = 300+200*sin(ghost_frac*TWO_PI);
 		let cur_y = 200+100*sin(ghost_frac*4*PI);
 
 		drawCursor(cur_x,cur_y);
@@ -20,6 +39,18 @@ function draw_one_frame(cur_frac) {
 		stroke("red");
 		strokeWeight(4);
 		drawCursor(200,200);
+		drawWindow(100,100);
+	}
+}
+
+function drawWindow(x,y,td){
+	fill('#fa2a8b');
+	rect(x, y, 640/2, 360/2);
+	fill('white');
+	textSize(16);
+	textFont('Comic Sans MS');
+	if (td == 1) {
+		text(errors,x+20,y+50,580/2,250/2);
 	}
 }
 
